9c07a1662fc99d7928f006806a0cfb7369060c4e
ah-y <sugiura130418@icloud.com>
2022-09-04 18:13:37 +0900

    created doc/

---
+++ doc/computer_science.md
@@ -0,0 +1,50 @@
+# Computer Science
+
+---
+
+## Mac
+
+### Battery Remain on cli
+
+`pmset -g batt` outputs battery percentage
+
+---
+
+## Shell
+
+### Symbolic link tip
+
+When you use `ln -s`, arguments have to be **absolute** path.
+Make sure that not relying on current path.
+
+### Makefile
+
+Makefile is make's config file. Make sure that name is not MakeFile.
+This is invalid name so simply not working.
+
+### LLDB
+
+lldb's help is very useful. Use actively.
+
+### Useful
+
+>***Environment Variable***
+
+- `XDG_CONFIG_HOME` (default is $HOME/.config)
+
+>***Useful Shell Command***
+
+- `date`
+date command prints current time, day, year, day of the week on certain time zone
+- `builtin <command> [option]`
+execute builtin command
+
+---
+
+## Rust
+
+### `pub(path)` conjection
+
+>see [official reference](https://doc.rust-lang.org/reference/visibility-and-privacy.html#pubin-path-pubcrate-pubsuper-and-pubself), [related error](https://doc.rust-lang.org/error-index.html#E0704), [my playground example](https://github.com/ah-y/playground/blob/master/rust/elseeee/src/main.rs)
+
+Since 2018 Edition, `path` for `pub(path)` must start with crate, super.
--- rust/elseeee/src/main.rs
+++ rust/elseeee/src/main.rs
@@ -1,206 +1,221 @@
 #![allow(unused)]
-
 //!Documentation for crate
 use std::{
-   any::{Any, TypeId},
-   env::args,
+	any::{Any, TypeId},
+	env::args,
 };
-
 ///Documentation
 fn main() {
-   //Rust can define function in function, return closure intuitive way by using
-   // impl trait.
+	//Rust can define function in function, return closure intuitive way by using
+	// impl trait.
 
-   ///This returns closure
-   fn return_closure() -> impl Fn() -> i32 {
-      fn fn_in_main() -> impl Fn(String,) -> String { |x| x }
-      || fn_in_main()("7".to_string(),).parse::<i32>().unwrap()
-   }
+	///This returns closure
+	fn return_closure() -> impl Fn() -> i32 {
+		fn fn_in_main() -> impl Fn(String,) -> String { |x| x }
+		|| {
+			fn_in_main()("7".to_string(),)
+				.parse::<i32>()
+				.unwrap()
+		}
+	}
 
-   println!("{}", return_closure()());
+	println!(
+		"{}",
+		return_closure()()
+	);
 
-   //How rust identify closure itself and closure's return value in fn's argument
-   fn closure_ornot<GenT: 'static,>(_which: GenT,) {
-      //typeid of generic parameter's type
-      let gen_id = TypeId::of::<GenT,>();
-      if TypeId::of::<i32,>() == gen_id {
-         println!("GenT is i32");
-      } else if TypeId::of::<dyn Fn() -> i32,>() == gen_id {
-         println!("GenT is Fn()->i32");
-      } else {
-         println!("unexpected!!");
-      }
-   }
+	//How rust identify closure itself and closure's return value in fn's argument
+	fn closure_ornot<GenT: 'static,>(_which: GenT,) {
+		//typeid of generic parameter's type
+		let gen_id = TypeId::of::<GenT,>();
+		if TypeId::of::<i32,>() == gen_id {
+			println!("GenT is i32");
+		} else if TypeId::of::<dyn Fn() -> i32,>() == gen_id {
+			println!("GenT is Fn()->i32");
+		} else {
+			println!("unexpected!!");
+		}
+	}
 
-   closure_ornot(return_closure()(),);
+	closure_ornot(return_closure()(),);
 
-   //closure's type is different even arguments and return types are same this
-   // call's "anonymous type"
-   let cl1 = || 1;
-   let cl2 = || 2;
-   println!("{}", cl1.type_id() == cl2.type_id());
+	//closure's type is different even arguments and return types are same this
+	// call's "anonymous type"
+	let cl1 = || 1;
+	let cl2 = || 2;
+	println!(
+		"{}",
+		cl1.type_id() == cl2.type_id()
+	);
 
-   //fn's type is same if types of all arguments and return value are same
-   fn ret1() -> i32 { 1 }
-   fn ret2() -> i32 { 2 }
-   println!("{}", ret1.type_id() == ret2.type_id());
+	//fn's type is same if types of all arguments and return value are same
+	fn ret1() -> i32 { 1 }
+	fn ret2() -> i32 { 2 }
+	println!(
+		"{}",
+		ret1.type_id() == ret2.type_id()
+	);
 
-   ///=============================================================
+	///=============================================================
 
-   //rust's trait has alot advanced features:D
-   trait MyName {
-      fn is(&self,);
-   }
+	//rust's trait has alot advanced features:D
+	trait MyName {
+		fn is(&self,);
+	}
 
-   impl<T,> MyName for Vec<T,> {
-      fn is(&self,) {
-         println!("!Vec<T>!");
-      }
-   }
+	impl<T,> MyName for Vec<T,> {
+		fn is(&self,) {
+			println!("!Vec<T>!");
+		}
+	}
 
-   impl MyName for i32 {
-      fn is(&self,) {
-         println!("!int!");
-      }
-   }
+	impl MyName for i32 {
+		fn is(&self,) {
+			println!("!int!");
+		}
+	}
 
-   impl<T,> MyName for (i32, Vec<T,>,) {
-      fn is(&self,) {
-         println!("!(i32, Vec<T>)!");
-      }
-   }
+	impl<T,> MyName for (i32, Vec<T,>,) {
+		fn is(&self,) {
+			println!("!(i32, Vec<T>)!");
+		}
+	}
 
-   let v = vec![0, 1, 1, 2, 23];
-   let ai = 0;
-   v.is();
-   ai.is();
-   (ai, v,).is();
+	let v = vec![0, 1, 1, 2, 23];
+	let ai = 0;
+	v.is();
+	ai.is();
+	(ai, v,).is();
 
-   //methods (in this case, is()) are only able to use within defined crate.
-   // This guarantees safety
+	//methods (in this case, is()) are only able to use within defined crate.
+	// This guarantees safety
 
-   ///===============================================================
+	///===============================================================
 
-   //experiment of behavior of std::io::. Whether read_line rewrite buf or not?
-   /*
-   let mut buf = String::new();
-   let _ = std::io::stdin().read_line(&mut buf,);
-   let _ = std::io::stdin().read_line(&mut buf,);
-   println!("{buf}");
-   */
+	//experiment of behavior of std::io::. Whether read_line rewrite buf or not?
+	/*
+	let mut buf = String::new();
+	let _ = std::io::stdin().read_line(&mut buf,);
+	let _ = std::io::stdin().read_line(&mut buf,);
+	println!("{buf}");
+	*/
 
-   ///==============================================================
-   //Option<T> supports iterator
-   let a = Some("a",);
-   for i in a.iter() {
-      println!("{i}");
-   }
+	///==============================================================
+	//Option<T> supports iterator
+	let a = Some("a",);
+	for i in a.iter() {
+		println!("{i}");
+	}
 
-   ///===============================================================
+	///===============================================================
 
-   //Rust's pattern matching arm & @ binding
-   fn odd(i: i32,) -> bool {
-      if i % 2 == 0 {
-         true
-      } else {
-         false
-      }
-   }
+	//Rust's pattern matching arm & @ binding
+	fn odd(i: i32,) -> bool {
+		if i % 2 == 0 {
+			true
+		} else {
+			false
+		}
+	}
 
-   match 3 {
-      c @ (4..=8 | 33) => println!("{c} is 4..=8 or 33"),
-      c if odd(c,) => println!("{c} is odd"),
-      666 if false => println!("†666†"),
-      c @ (11 | 22) => println!("{c} is 11 or 22"),
-      _ => println!("I know that I know nothing"),
-   }
+	match 3 {
+		c @ (4..=8 | 33) => println!("{c} is 4..=8 or 33"),
+		c if odd(c,) => println!("{c} is odd"),
+		666 if false => println!("†666†"),
+		c @ (11 | 22) => println!("{c} is 11 or 22"),
+		_ => println!("I know that I know nothing"),
+	}
 
-   //if keyword in match arm & @ binding is similar, but following example is
-   // not compliable
-   /*
-   match 3 {
-      c if (4..=8 | 33) => println!("{c} is 4..=8 or 33"),
-      c @ odd(c,) => println!("{c} is odd"),
-      666 @ false => println!("†666†"),
-      c if (11 | 22) => println!("{c} is 11 or 22"),
-      _ => println!("I know that I know nothing"),
-   }
-   */
+	//if keyword in match arm & @ binding is similar, but following example is
+	// not compliable
+	/*
+	match 3 {
+	   c if (4..=8 | 33) => println!("{c} is 4..=8 or 33"),
+	   c @ odd(c,) => println!("{c} is odd"),
+	   666 @ false => println!("†666†"),
+	   c if (11 | 22) => println!("{c} is 11 or 22"),
+	   _ => println!("I know that I know nothing"),
+	}
+	*/
 
-   ///===============================================================
-   //Option::map uses raw-value if is Some().
-   //But how about self is None?
-   let mut some_none = None;
-   some_none.map(|_one| panic!("This painc shouldn't be executed"),);
-   some_none = Some(1,);
-   some_none.map(|one| println!("{one}"),);
+	///===============================================================
+	//Option::map uses raw-value if is Some().
+	//But how about self is None?
+	let mut some_none = None;
+	some_none.map(|_one| panic!("This painc shouldn't be executed"),);
+	some_none = Some(1,);
+	some_none.map(|one| println!("{one}"),);
 
-   ///===============================================================
-   //Checking idea that returning private method's pointer enables to access
-   // private method Result is bad at rust-nightly 1.64.0
-   let has_prv = HasPrivate { pub_member: 0, private_member: 0, };
-   has_prv.pub_f()();
+	///===============================================================
+	//Checking idea that returning private method's pointer enables to access
+	// private method Result is bad at rust-nightly 1.64.0
+	let has_prv = HasPrivate { pub_member: 0, private_member: 0, };
+	has_prv.pub_f()();
 
-   //Sort result between '-' & alphanumerics
-   let mut string_vecs: Vec<&str,> = vec!["--options", "-h", "--help", "a", "z", "0", "9", "A", "Z"];
-   string_vecs.sort();
-   println!("{:?}", string_vecs);
+	//Sort result between '-' & alphanumerics
+	let mut string_vecs: Vec<&str,> =
+		vec!["--options", "-h", "--help", "a", "z", "0", "9", "A", "Z"];
+	string_vecs.sort();
+	println!(
+		"{:?}",
+		string_vecs
+	);
 
-   ///===============================================================
-   //Experiment pub(path)'s behavior
-   mod1::allowed_view();
+	///===============================================================
+	//Experiment pub(path)'s behavior
+	mod1::allowed_view();
 
-   ///===============================================================
-   //env::arg
-   let argsss = args();
-   println!("{:?}", argsss);
-   let argu = args();
-   println!("{:?}", argu);
+	///===============================================================
+	//env::arg
+	let argsss = args();
+	println!("{:?}", argsss);
+	let argu = args();
+	println!("{:?}", argu);
 }
 
 struct HasPrivate {
-   pub pub_member: usize,
-   private_member: usize,
+	pub pub_member: usize,
+	private_member: usize,
 }
 impl HasPrivate {
-   pub fn pub_f(self,) -> impl Fn() -> () {
-      //self.prv_fn | this cause error. compiler recognize as field, not method
-      || println!("from pub_f")
-   }
+	pub fn pub_f(self,) -> impl Fn() -> () {
+		//self.prv_fn | this cause error. compiler recognize as field, not method
+		|| println!("from pub_f")
+	}
 
-   fn prv_fn(self,) { println!("in plivate function") }
+	fn prv_fn(self,) { println!("in plivate function") }
 }
 
 pub mod mod1 {
-   //!Documentation for module
-   pub mod mod2 {
-      pub(in crate::mod1) fn visible() {
-         println!("mod1::mod2::visible()");
-      }
+	//!Documentation for module
+	pub mod mod2 {
+		pub(in crate::mod1) fn visible() {
+			println!("mod1::mod2::visible()");
+		}
 
-      pub(in crate::mod1::mod2) fn only_in_mod3() {
-         println!("mod1::only_in_mod3()");
-      }
+		pub(in crate::mod1::mod2) fn only_in_mod3() {
+			println!("mod1::only_in_mod3()");
+		}
 
-      pub mod mod3 {
-         fn in_mod3() {}
-      }
-   }
+		pub mod mod3 {
+			fn in_mod3() {}
+		}
+	}
 
-   pub mod mod4 {
-      pub mod mod5 {
-         fn private_fn() {}
-      }
-   }
+	pub mod mod4 {
+		pub mod mod5 {
+			fn private_fn() {}
+		}
+	}
 
-   pub fn allowed_view() {
-      println!("calling from mod1::allowed_view()---------");
-      mod2::visible();
-   }
+	pub fn allowed_view() {
+		println!("calling from mod1::allowed_view()---------");
+		mod2::visible();
+	}
 
-   pub struct InMod1 {}
+	pub struct InMod1 {}
 }
 
 pub mod mod6 {
-   pub fn in_mod6() {}
+	pub fn in_mod6() {}
 }
