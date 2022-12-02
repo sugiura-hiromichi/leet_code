49e2515647ff2a0db494b1c115e144e9c885a9d6
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-02 12:07:45 +0900

    TLE(23): TLE means Time Limit Exceeded

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -1,26 +1,43 @@
 #![allow(unused, dead_code)]
+#[derive(PartialEq, Eq, Clone, Debug,)]
+pub struct ListNode {
+	pub val:  i32,
+	pub next: Option<Box<ListNode,>,>,
+}
+
+impl ListNode {
+	#[inline]
+	fn new(val: i32,) -> Self { ListNode { next: None, val, } }
+}
 
 struct Solution;
 impl Solution {
-	pub fn generate_parenthesis(n: i32,) -> Vec<String,> { back_track("".to_string(), n, 0,) }
+	pub fn merge_k_lists(lists: Vec<Option<Box<ListNode,>,>,>,) -> Option<Box<ListNode,>,> {
+		let mut rslt = None;
+		for l in lists {
+			rslt = merge_two_lists(rslt, l,);
+		}
+		rslt
+	}
 }
 
-fn back_track(s: String, open: i32, close: i32,) -> Vec<String,> {
-	if open == 0 && close == 0 {
-		return vec![s];
+fn merge_two_lists(
+	l1: Option<Box<ListNode,>,>,
+	l2: Option<Box<ListNode,>,>,
+) -> Option<Box<ListNode,>,> {
+	match (l1.clone(), l2.clone(),) {
+		(Some(mut one,), Some(mut two,),) => {
+			if one.val < two.val {
+				//l1.as_mut().unwrap().next = merge_two_lists(one.next, l2,);
+				Some(Box::new(ListNode { val: one.val, next: merge_two_lists(one.next, l2,), },),)
+			} else {
+				//l2.as_mut().unwrap().next = merge_two_lists(l1, two.next,);
+				Some(Box::new(ListNode { val: two.val, next: merge_two_lists(two.next, l1,), },),)
+			}
+		},
+		(nxt, None,) | (None, nxt,) => nxt,
 	}
-
-	let mut ret = vec![];
-	if open > 0 {
-		ret.append(&mut back_track(s.clone() + "(", open - 1, close + 1,),);
-	}
-	if close > 0 {
-		ret.append(&mut back_track(s.clone() + ")", open, close - 1,),);
-	}
-	ret
 }
-
-fn main() {}
 
 #[cfg(test)]
 mod tests {
@@ -28,23 +45,37 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = vec!["((()))", "(()())", "(())()", "()(())", "()()()"];
-		let mut sol = Solution::generate_parenthesis(3,);
-
-		ans.sort();
-		sol.sort();
-
+		let mut ans = ary_to_list(&[1, 1, 2, 3, 4, 4, 5, 6,],);
+		let mut sol =
+			Solution::merge_k_lists(arys_to_lists(vec![&[1, 4, 5,], &[1, 3, 4,], &[2, 6,]],),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec!["()"];
-		let mut sol = Solution::generate_parenthesis(1,);
-
-		ans.sort();
-		sol.sort();
-
+		let mut ans = ary_to_list(&[],);
+		let mut sol = Solution::merge_k_lists(arys_to_lists(vec![],),);
 		assert_eq!(ans, sol);
 	}
+
+	#[test]
+	fn test_3() {
+		let mut ans = ary_to_list(&[],);
+		let mut sol = Solution::merge_k_lists(arys_to_lists(vec![&[]],),);
+		assert_eq!(ans, sol);
+	}
+
+	fn ary_to_list(ary: &[i32],) -> Option<Box<ListNode,>,> {
+		if ary.is_empty() {
+			None
+		} else {
+			Some(Box::new(ListNode { val: ary[0], next: ary_to_list(&ary[1..],), },),)
+		}
+	}
+
+	fn arys_to_lists(arys: Vec<&[i32],>,) -> Vec<Option<Box<ListNode,>,>,> {
+		arys.iter().map(|&a| ary_to_list(a,),).collect()
+	}
 }
+
+fn main() {}
