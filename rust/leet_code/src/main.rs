245cca54781a148c5b554ed7f1cdf199cb729e90
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-06 09:03:24 +0900

    accepted(24): recursion

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -13,16 +13,17 @@ impl ListNode {
 }
 
 impl Solution {
-	pub fn merge_k_lists(mut lists: Vec<Option<Box<ListNode,>,>,>,) -> Option<Box<ListNode,>,> {
-		let i = lists
-			.iter()
-			.enumerate()
-			.min_by_key(|(_, x,)| x.as_ref().map_or(std::i32::MAX, |x| x.val,),)?
-			.0;
-		let mut head = lists[i].take()?;
-		lists[i] = head.next;
-		head.next = Self::merge_k_lists(lists,);
-		Some(head,)
+	pub fn swap_pairs(mut head: Option<Box<ListNode,>,>,) -> Option<Box<ListNode,>,> {
+		let next_head = match head.clone() {
+			Some(n,) if n.next.is_some() => n.clone().next.unwrap().next,
+			_ => return head,
+		};
+
+		let tmp = head.clone();
+		head = head.as_mut().unwrap().next.as_ref().cloned();
+		head.as_mut().unwrap().next = tmp;
+		head.as_mut().unwrap().next.as_mut().unwrap().next = Solution::swap_pairs(next_head,);
+		head
 	}
 }
 
@@ -33,29 +34,28 @@ mod tests {
 	#[test]
 	fn test_1() {
 		let mut ans = ary_to_list(&[1, 1, 2, 3, 4, 4, 5, 6,],);
-		let mut sol =
-			Solution::merge_k_lists(arys_to_lists(vec![&[1, 4, 5,], &[1, 3, 4,], &[2, 6,]],),);
+		let mut sol = Solution::swap_pairs(ary_to_list(&[1, 1, 3, 2, 4, 4, 6, 5,],),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = ary_to_list(&[],);
-		let mut sol = Solution::merge_k_lists(arys_to_lists(vec![],),);
+		let mut ans = ary_to_list(&[2, 1, 4, 3,],);
+		let mut sol = Solution::swap_pairs(ary_to_list(&[1, 2, 3, 4,],),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_3() {
 		let mut ans = ary_to_list(&[],);
-		let mut sol = Solution::merge_k_lists(arys_to_lists(vec![&[]],),);
+		let mut sol = Solution::swap_pairs(ary_to_list(&[],),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_4() {
-		let mut ans = ary_to_list(&[1, 2, 3,],);
-		let mut sol = Solution::merge_k_lists(arys_to_lists(vec![&[1,], &[2, 3,]],),);
+		let mut ans = ary_to_list(&[1,],);
+		let mut sol = Solution::swap_pairs(ary_to_list(&[1,],),);
 		assert_eq!(ans, sol);
 	}
 
