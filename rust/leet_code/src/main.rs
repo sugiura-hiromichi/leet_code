7f77ea1c9d8ed492591c15be506ec9a82a8047ae
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-10 08:30:43 +0900

    solutions(25): recursion with more simple logic

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -1,4 +1,5 @@
-#![allow(unused, dead_code)]
+#![allow(unused)]
+
 struct Solution;
 
 #[derive(PartialEq, Eq, Clone, Debug,)]
@@ -13,18 +14,43 @@ impl ListNode {
 }
 
 impl Solution {
-	pub fn swap_pairs(mut head: Option<Box<ListNode,>,>,) -> Option<Box<ListNode,>,> {
-		let next_head = match head.clone() {
-			Some(n,) if n.next.is_some() => n.clone().next.unwrap().next,
-			_ => return head,
-		};
+	pub fn reverse_k_group(mut head: Option<Box<ListNode,>,>, k: i32,) -> Option<Box<ListNode,>,> {
+		let mut cur = head.clone();
 
-		let tmp = head.clone();
-		head = head.as_mut().unwrap().next.as_ref().cloned();
-		head.as_mut().unwrap().next = tmp;
-		head.as_mut().unwrap().next.as_mut().unwrap().next = Solution::swap_pairs(next_head,);
+		// find k+1th node
+		let mut i = 0;
+		while cur != None && i != k {
+			cur = cur?.next;
+			i += 1;
+		}
+
+		if i == k {
+			// reverse rest k groups with k+1th node as head
+			cur = Solution::reverse_k_group(cur, k,);
+
+			// reverse current k group
+			for _ in 0..i {
+				let tmp = head.clone()?.next;
+				head.as_mut()?.next = cur; // create reversed k+1-ith node
+				cur = head.clone();
+				head = tmp;
+			}
+			head = cur;
+		}
 		head
 	}
+}
+
+fn ary_to_list(ary: &[i32],) -> Option<Box<ListNode,>,> {
+	if ary.is_empty() {
+		None
+	} else {
+		Some(Box::new(ListNode { val: ary[0], next: ary_to_list(&ary[1..],), },),)
+	}
+}
+
+fn arys_to_lists(arys: Vec<&[i32],>,) -> Vec<Option<Box<ListNode,>,>,> {
+	arys.iter().map(|&a| ary_to_list(a,),).collect()
 }
 
 #[cfg(test)]
@@ -33,43 +59,36 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = ary_to_list(&[1, 1, 2, 3, 4, 4, 5, 6,],);
-		let mut sol = Solution::swap_pairs(ary_to_list(&[1, 1, 3, 2, 4, 4, 6, 5,],),);
+		let mut ans = ary_to_list(&[5, 4, 3, 2, 1,],);
+		let mut sol = Solution::reverse_k_group(ary_to_list(&[1, 2, 3, 4, 5,],), 5,);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = ary_to_list(&[2, 1, 4, 3,],);
-		let mut sol = Solution::swap_pairs(ary_to_list(&[1, 2, 3, 4,],),);
+		let mut ans = ary_to_list(&[3, 2, 1, 4, 5,],);
+		let mut sol = Solution::reverse_k_group(ary_to_list(&[1, 2, 3, 4, 5,],), 3,);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_3() {
-		let mut ans = ary_to_list(&[],);
-		let mut sol = Solution::swap_pairs(ary_to_list(&[],),);
+		let mut ans = ary_to_list(&[3, 2, 1,],);
+		let mut sol = Solution::reverse_k_group(ary_to_list(&[1, 2, 3,],), 3,);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_4() {
-		let mut ans = ary_to_list(&[1,],);
-		let mut sol = Solution::swap_pairs(ary_to_list(&[1,],),);
+		let mut ans = ary_to_list(&[4, 3, 2, 1,],);
+		let mut sol = Solution::reverse_k_group(ary_to_list(&[1, 2, 3, 4,],), 4,);
 		assert_eq!(ans, sol);
-	}
-
-	fn ary_to_list(ary: &[i32],) -> Option<Box<ListNode,>,> {
-		if ary.is_empty() {
-			None
-		} else {
-			Some(Box::new(ListNode { val: ary[0], next: ary_to_list(&ary[1..],), },),)
-		}
-	}
-
-	fn arys_to_lists(arys: Vec<&[i32],>,) -> Vec<Option<Box<ListNode,>,>,> {
-		arys.iter().map(|&a| ary_to_list(a,),).collect()
 	}
 }
 
-fn main() {}
+// use only when stdin is needed
+fn main() {
+	let mut ans = ary_to_list(&[5, 4, 3, 2, 1,],);
+	let mut sol = Solution::reverse_k_group(ary_to_list(&[1, 2, 3, 4, 5,],), 5,);
+	assert_eq!(ans, sol);
+}
