c6f1bc0562cc8707ee8635964f019e83394f315a
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-12 08:32:58 +0900

    accepted(29): rt33main.rs mem61main.rs

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -13,16 +13,58 @@ impl ListNode {
 	fn new(val: i32,) -> Self { ListNode { next: None, val, } }
 }
 
+trait AltAbs {
+	fn minus_abs(self,) -> Self;
+}
+
+impl AltAbs for (i32, i32,) {
+	fn minus_abs(self,) -> Self {
+		let a = if self.0 > 0 { -self.0 } else { self.0 };
+		let b = if self.1 > 0 { -self.1 } else { self.1 };
+		(a, b,)
+	}
+}
+
 impl Solution {
-	pub fn remove_element(nums: &mut Vec<i32,>, val: i32,) -> i32 {
-		let mut insert_index = 0;
-		for i in 0..nums.len() {
-			if nums[i] != val {
-				nums[insert_index] = nums[i];
-				insert_index += 1;
+	pub fn divide(dividend: i32, divisor: i32,) -> i32 {
+		if divisor == -1 {
+			if dividend == i32::MIN {
+				return i32::MAX;
+			} else {
+				return -dividend;
+			}
+		} else if divisor == 1 {
+			return dividend;
+		}
+		if dividend == 0 {
+			return 0;
+		}
+
+		// d: dd & ds are minus
+		let (dd, ds,) = (dividend, divisor,).minus_abs();
+		let mut no_frac = dd;
+
+		// q: sub from dd and check if dd<0
+		let mut subs = vec![(ds, 1,)];
+		let mut tmp = (ds, 1,);
+		while !tmp.0.overflowing_add(tmp.0,).1 && no_frac < tmp.0 {
+			tmp = (tmp.0 + tmp.0, tmp.1 + tmp.1,);
+			subs.push(tmp,);
+		}
+
+		let mut quo = 0;
+		for (sub, qs,) in subs.iter().rev() {
+			while no_frac <= *sub {
+				no_frac -= sub;
+				quo += qs;
 			}
 		}
-		insert_index as i32
+
+		if dividend > 0 && divisor > 0 || dividend < 0 && divisor < 0 {
+			quo
+		} else {
+			-quo
+		}
 	}
 }
 
@@ -44,20 +86,44 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let nums = &mut vec![3, 2, 2, 3];
-		let mut ans = 2;
-		let mut sol = Solution::remove_element(nums, 3,);
+		let mut ans = 3;
+		let mut sol = Solution::divide(10, 3,);
 		assert_eq!(ans, sol);
-		assert_eq!(nums, &[2, 2]);
 	}
 
 	#[test]
 	fn test_2() {
-		let nums = &mut vec![0, 1, 2, 2, 3, 0, 4, 2];
-		let mut ans = 5;
-		let mut sol = Solution::remove_element(nums, 2,);
+		let mut ans = -2;
+		let mut sol = Solution::divide(7, -3,);
 		assert_eq!(ans, sol);
-		assert_eq!(nums, &[0, 1, 3, 0, 4]);
+	}
+
+	#[test]
+	fn test_3() {
+		let mut ans = 2147483647;
+		let mut sol = Solution::divide(2147483647, 1,);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_4() {
+		let mut ans = 1073741823;
+		let mut sol = Solution::divide(2147483647, 2,);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_5() {
+		let mut ans = 1;
+		let mut sol = Solution::divide(2, 2,);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_6() {
+		let mut ans = -1073741824;
+		let mut sol = Solution::divide(-2147483648, 2,);
+		assert_eq!(ans, sol);
 	}
 }
 
@@ -65,19 +131,19 @@ mod tests {
 fn main() {
 	// todo-comments
 	// FIX:
-	// e:
+	// e: `e` stands for "error"
 	// TODO:
-	// q:
+	// q: `q` stands for "question"
 	// HACK:
-	// a:
+	// a: `a` stands for "attention"
 	// WARN:
-	// x:
+	// x: `x` is abbreviation of "XXX"
 	// PERF:
-	// p:
+	// p: `p` stands for "performance"
 	// NOTE:
-	// d:
+	// d: `d` stands for "description"
 	// TEST:
-	// t:
+	// t: `t` stands for "test"
 	// PASS:
 	// FAIL:
 }
