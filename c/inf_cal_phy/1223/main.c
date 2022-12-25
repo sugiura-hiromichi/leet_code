c0764d374d9eec6fd7e45c61fb88a922be2f7e67
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-25 17:40:59 +0900

    finished(rep_3): c/inf_cal_phy/1223

--- c/inf_cal_phy/1223/main.c
+++ c/inf_cal_phy/1223/main.c
@@ -1,8 +1,10 @@
+#include <assert.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define EPSILON .000000001
+#define DELTA .00000001
 #define DELTA_T .05
 #define ECCENTRICITY .5
 
@@ -23,7 +25,7 @@ double newton_kepler(double theta, double t) {
   double prev;
   do {
     prev = theta;
-    theta = -f(theta, t) / f_dash(theta, t);
+    theta = -f(theta, t) / f_dash(theta, t) + theta;
   } while (fabs(theta - prev) > EPSILON);
 
   return theta;
@@ -33,8 +35,17 @@ int main() {
   // question1
   double t = .25, cur = .5;
   double sol = newton_kepler(cur, t);
+  assert(sol - 0.4815980 < DELTA);
 
   // question2
   // NOTE: T=2π
-  double cycle_len = 2 * M_PI;
+  cur = .0, t = .0;
+  double cycle_len = 2.0 * M_PI;
+  for (int i = 0; t < cycle_len; i++) {
+    printf("%d %e %e %e %e\n", i, t, cur, cos(cur) - ECCENTRICITY,
+           sqrt(1 - ECCENTRICITY * ECCENTRICITY) * sin(cur));
+
+    t += DELTA_T;
+    cur = newton_kepler(cur, t);
+  }
 }
