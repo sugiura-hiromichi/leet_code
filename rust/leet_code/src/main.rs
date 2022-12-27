5e6142241dab73b5c2e50aeb437f86edf4b569e4
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-27 16:01:33 +0900

    tle(39) using recursion

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -3,13 +3,31 @@
 struct Solution;
 impl Solution {
 	pub fn combination_sum(candidates: Vec<i32,>, target: i32,) -> Vec<Vec<i32,>,> {
-		let mut ret = vec![];
-		for i in 0..candidates.len() {
-			let mut max = i;
+		let mut ret = std::collections::HashSet::new();
+		let mut n = 1;
+		while n <= target / 2 {
+			let n_sum = Self::combination_sum(candidates.clone(), n,);
+			let t_sub_n = Self::combination_sum(candidates.clone(), target - n,);
+
+			if !n_sum.is_empty() && !t_sub_n.is_empty() {
+				for n in n_sum {
+					for t_n in t_sub_n.iter() {
+						let mut v = [n.clone(), t_n.clone(),].concat();
+						v.sort();
+						ret.insert(v,);
+					}
+				}
+			}
+			n += 1;
 		}
 
-		// q:
-		todo!()
+		for i in 0..candidates.len() {
+			if candidates[i] == target {
+				ret.insert(vec![target],);
+				break;
+			}
+		}
+		ret.iter().map(|v| v.clone(),).collect()
 	}
 }
 
@@ -35,6 +53,13 @@ mod tests {
 	fn test_3() {
 		let mut ans: Vec<Vec<i32,>,> = vec![];
 		let mut sol = Solution::combination_sum(vec![2], 1,);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_4() {
+		let mut ans: Vec<Vec<i32,>,> = vec![];
+		let mut sol = Solution::combination_sum(vec![5, 10, 8, 4, 3, 12, 9], 27,);
 		assert_eq!(ans, sol);
 	}
 }
