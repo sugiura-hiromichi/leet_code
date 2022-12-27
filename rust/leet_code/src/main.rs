27c3a9d9238f6602df23ba5dcd646b423f937921
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-27 09:33:55 +0900

    solutions(37): more sophisticated code. same algorhytm

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -2,87 +2,54 @@
 
 struct Solution;
 impl Solution {
-	pub fn solve_sudoku(board: &mut Vec<Vec<char,>,>,) -> bool {
-		// p: refactor not to use `has_dot`. keep coordinate info to variable.
-		// And use alternate `solve_sudoku(pos:(usize,usize), board: ~)` to recursion
-		while let Some(t,) = has_dot(board,) {
-			let mut validated = self_valid(t, board,);
-			if validated == 0 {
-				return false;
-			}
+	pub fn solve_sudoku(board: &mut Vec<Vec<char,>,>,) { Self::solve_sudoku_helper(board, 0,); }
 
-			let mut th = 0;
-			while validated != 0 {
-				th += 1;
-				validated = validated >> 1;
-				if validated & 1 == 1 {
-					board[t.0][t.1] = char::from_digit(th, 10,).unwrap();
-					let mut b_clone = board.clone();
-					if Self::solve_sudoku(&mut b_clone,) {
-						*board = b_clone;
-						return true;
-					}
-				}
-			}
-			// d: if program reach here, all valid number were invalid
-			return false;
+	fn solve_sudoku_helper(board: &mut Vec<Vec<char,>,>, n: usize,) -> bool {
+		if n == 81 {
+			return true;
 		}
-		true
-	}
-}
 
-/// returns `(yoko, tate)`
-fn has_dot(board: &Vec<Vec<char,>,>,) -> Option<(usize, usize,),> {
-	for i in 0..9 {
-		for j in 0..9 {
-			if board[i][j] == '.' {
-				return Some((i, j,),);
-			}
+		let (i, j,) = (n / 9, n % 9,);
+		if board[i][j] != '.' {
+			return Self::solve_sudoku_helper(board, n + 1,);
 		}
-	}
-	None
-}
 
-/// returns valid flag to the position of `t`
-/// a: take care that flag is set **1~9th**. **NOT 0~8th!**.
-fn self_valid(t: (usize, usize,), board: &Vec<Vec<char,>,>,) -> u16 {
-	yoko_valid(t, board,) & tate_valid(t, board,) & block_valid(t, board,)
-}
-
-fn yoko_valid(t: (usize, usize,), board: &Vec<Vec<char,>,>,) -> u16 {
-	let mut is_valid = 0b1111111110u16;
-	for i in 0..3 {
-		for j in 0..3 {
-			if let Some(c,) = board[t.0][i * 3 + j].to_digit(10,) {
-				is_valid &= !(1 << c);
+		let mask = Self::check(board, i, j,);
+		for b in 1..=9 {
+			if (mask >> b) & 1 == 1 {
+				continue;
 			}
+			board[i][j] = std::char::from_digit(b, 10,).unwrap();
+			if Self::solve_sudoku_helper(board, n + 1,) {
+				return true;
+			}
+			board[i][j] = '.';
+		}
+		false
+	}
+
+	fn check(board: &Vec<Vec<char,>,>, i: usize, j: usize,) -> u16 {
+		let mut res = 0;
+		for n in 0..9 {
+			let rc = board[i][n];
+			let cc = board[n][j];
+			let sc = board[i / 3 * 3 + n / 3][(j / 3 * 3) + (n % 3)];
+			Self::mask(&mut res, rc,);
+			Self::mask(&mut res, cc,);
+			Self::mask(&mut res, sc,);
+		}
+		res
+	}
+
+	#[inline]
+	fn mask(x: &mut u16, c: char,) {
+		match c {
+			'.' => {},
+			c => {
+				*x |= 1 << (c.to_digit(10,).unwrap());
+			},
 		}
 	}
-	is_valid
-}
-
-fn tate_valid(t: (usize, usize,), board: &Vec<Vec<char,>,>,) -> u16 {
-	let mut is_valid = 0b1111111110u16;
-	for i in 0..3 {
-		for j in 0..3 {
-			if let Some(c,) = board[i * 3 + j][t.1].to_digit(10,) {
-				is_valid &= !(1 << c);
-			}
-		}
-	}
-	is_valid
-}
-
-fn block_valid(t: (usize, usize,), board: &Vec<Vec<char,>,>,) -> u16 {
-	let mut is_valid = 0b1111111110u16;
-	for i in 0..3 {
-		for j in 0..3 {
-			if let Some(c,) = board[t.0 / 3 * 3 + i][t.1 / 3 * 3 + j].to_digit(10,) {
-				is_valid &= !(1 << c);
-			}
-		}
-	}
-	is_valid
 }
 
 #[cfg(test)]
@@ -115,89 +82,6 @@ mod tests {
 		];
 		Solution::solve_sudoku(&mut sol,);
 		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn arithmetic() {
-		assert_eq!(2, 3 / 2 * 2);
-		let mut is_valid = [0u16; 1];
-		assert_eq!(is_valid, [0]);
-	}
-
-	#[test]
-	fn test_self_valid() {
-		let t = (0, 2,);
-		let mut board = vec![
-			vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-			vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-			vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-			vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-			vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-			vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-			vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-			vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-			vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
-		];
-		let sol = self_valid(t, &board,);
-		let ans = 0b0000010110;
-		assert_eq!(sol, ans);
-	}
-
-	#[test]
-	fn test_yoko_valid() {
-		let t = (0, 2,);
-		let mut board = vec![
-			vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-			vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-			vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-			vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-			vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-			vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-			vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-			vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-			vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
-		];
-		let sol = yoko_valid(t, &board,);
-		let ans = 0b1101010110;
-		assert_eq!(sol, ans);
-	}
-
-	#[test]
-	fn test_tate_valid() {
-		let t = (0, 2,);
-		let mut board = vec![
-			vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-			vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-			vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-			vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-			vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-			vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-			vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-			vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-			vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
-		];
-		let sol = tate_valid(t, &board,);
-		let ans = 0b1011111110;
-		assert_eq!(sol, ans);
-	}
-
-	#[test]
-	fn test_block_valid() {
-		let t = (0, 2,);
-		let mut board = vec![
-			vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-			vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-			vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-			vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-			vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-			vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-			vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-			vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-			vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
-		];
-		let sol = block_valid(t, &board,);
-		let ans = 0b0010010110;
-		assert_eq!(sol, ans);
 	}
 }
 
