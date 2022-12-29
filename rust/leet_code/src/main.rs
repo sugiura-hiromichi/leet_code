6b3e336910b2649185c72496f5c886ee45b91ed1
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-29 09:50:13 +0900

    accepted(40) rt[0 100] mem[2.1 51]

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -2,7 +2,7 @@
 
 struct Solution;
 impl Solution {
-	pub fn combination_sum(candidates: Vec<i32,>, target: i32,) -> Vec<Vec<i32,>,> {
+	pub fn combination_sum2(candidates: Vec<i32,>, target: i32,) -> Vec<Vec<i32,>,> {
 		let mut can = candidates;
 		can.sort();
 		core_sum(can, target,)
@@ -10,29 +10,28 @@ impl Solution {
 }
 
 fn core_sum(can: Vec<i32,>, target: i32,) -> Vec<Vec<i32,>,> {
-	let mut ret = vec![];
-	let mut i = 0;
-	while i + 1 < can.len() && can[i] < target {
-		for n in 1..=target / can[i] {
-			if can[i] * n == target {
-				ret.push(vec![can[i]; n as usize],);
-			} else {
-				core_sum(can[i + 1..].to_vec(), target - n * can[i],).iter_mut().for_each(|v| {
-					for _ in 0..n {
-						v.push(can[i],);
-					}
-					ret.push(v.clone(),);
-				},);
+	let (mut ret, len,) = (vec![], can.len(),);
+	if can.is_empty() || target < can[0] {
+		return ret;
+	}
+
+	let mut same = 1;
+	while same < len && can[0] == can[same] {
+		same += 1;
+	}
+
+	if target % can[0] == 0 && target / can[0] <= same as i32 {
+		ret.push(vec![can[0]; (target / can[0]) as usize],);
+	}
+
+	for n in 0..=same {
+		for mut v in core_sum(can[same..].to_vec(), target - n as i32 * can[0],) {
+			for _ in 0..n {
+				v.push(can[0],);
 			}
+			ret.push(v,);
 		}
-
-		i += 1;
 	}
-
-	if 0 == target % can[i] {
-		ret.push(vec![can[i]; (target / can[i]) as usize],);
-	}
-
 	ret
 }
 
@@ -42,18 +41,17 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = vec![vec![2, 2, 3], vec![7]];
-		let mut sol = Solution::combination_sum(vec![2, 3, 6, 7], 7,);
+		let mut ans = vec![vec![1, 1, 6], vec![1, 2, 5], vec![1, 7], vec![2, 6]];
+		let mut sol = Solution::combination_sum2(vec![10, 1, 2, 7, 6, 1, 5], 8,);
 		sol.iter_mut().for_each(|v| v.sort(),);
 		sol.sort();
 		assert_eq!(ans, sol);
 	}
 
-	// FAIL: `sol==[3,5]`
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![2, 2, 2, 2], vec![2, 3, 3], vec![3, 5]];
-		let mut sol = Solution::combination_sum(vec![2, 3, 5], 8,);
+		let mut ans = vec![vec![1, 2, 2], vec![5]];
+		let mut sol = Solution::combination_sum2(vec![2, 5, 2, 1, 2], 5,);
 		sol.iter_mut().for_each(|v| v.sort(),);
 		sol.sort();
 		assert_eq!(ans, sol);
@@ -62,7 +60,7 @@ mod tests {
 	#[test]
 	fn test_3() {
 		let mut ans: Vec<Vec<i32,>,> = vec![];
-		let mut sol = Solution::combination_sum(vec![2], 1,);
+		let mut sol = Solution::combination_sum2(vec![1], 2,);
 		sol.iter_mut().for_each(|v| v.sort(),);
 		sol.sort();
 		assert_eq!(ans, sol);
@@ -70,8 +68,9 @@ mod tests {
 
 	#[test]
 	fn test_4() {
-		let mut ans: Vec<Vec<i32,>,> = vec![vec![2, 2, 2], vec![3, 3]];
-		let mut sol = Solution::combination_sum(vec![2, 3], 6,);
+		let mut ans =
+			vec![vec![1, 1, 2, 2], vec![1, 1, 4], vec![1, 2, 3], vec![2, 2, 2], vec![2, 4]];
+		let mut sol = Solution::combination_sum2(vec![4, 4, 2, 1, 4, 2, 2, 1, 3], 6,);
 		sol.iter_mut().for_each(|v| v.sort(),);
 		sol.sort();
 		assert_eq!(ans, sol);
