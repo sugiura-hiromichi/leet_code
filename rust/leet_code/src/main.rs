780065d69996a30c835ace8f62aa31a2ca52ffa2
sugiura-hiromichi <sugiura130418@icloud.com>
2022-12-29 16:34:00 +0900

    accepted(41) rt[7 69] mem[3 76]

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -1,38 +1,47 @@
 #![allow(unused)]
 
+/// this is doc comment.
+/// `hello` **how are you**
 struct Solution;
 impl Solution {
-	pub fn combination_sum2(candidates: Vec<i32,>, target: i32,) -> Vec<Vec<i32,>,> {
-		let mut can = candidates;
-		can.sort();
-		core_sum(can, target,)
-	}
-}
-
-fn core_sum(can: Vec<i32,>, target: i32,) -> Vec<Vec<i32,>,> {
-	let (mut ret, len,) = (vec![], can.len(),);
-	if can.is_empty() || target < can[0] {
-		return ret;
-	}
-
-	let mut same = 1;
-	while same < len && can[0] == can[same] {
-		same += 1;
-	}
-
-	if target % can[0] == 0 && target / can[0] <= same as i32 {
-		ret.push(vec![can[0]; (target / can[0]) as usize],);
-	}
-
-	for n in 0..=same {
-		for mut v in core_sum(can[same..].to_vec(), target - n as i32 * can[0],) {
-			for _ in 0..n {
-				v.push(can[0],);
-			}
-			ret.push(v,);
+	pub fn first_missing_positive(nums: Vec<i32,>,) -> i32 {
+		if nums.len() == 1 {
+			return if nums[0] == 1 { 2 } else { 1 };
 		}
+
+		let mut nums = nums;
+		nums.sort();
+		let (mut l, mut r,) = (0, nums.len(),);
+		if nums[r - 1] <= 0 {
+			return 1;
+		}
+
+		// find smallest positive number
+		while nums[l] <= 0 {
+			if l >= r {
+				return 1;
+			}
+			let mid = (l + r) / 2;
+			if nums[mid] <= 0 {
+				l = mid + 1;
+			} else {
+				r = mid;
+			}
+		}
+
+		if nums[l] > 1 {
+			return 1;
+		}
+
+		while l + 1 < nums.len() {
+			if nums[l + 1] - nums[l] > 1 {
+				return nums[l] + 1;
+			}
+			l += 1;
+		}
+
+		nums[l] + 1
 	}
-	ret
 }
 
 #[cfg(test)]
@@ -41,38 +50,22 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = vec![vec![1, 1, 6], vec![1, 2, 5], vec![1, 7], vec![2, 6]];
-		let mut sol = Solution::combination_sum2(vec![10, 1, 2, 7, 6, 1, 5], 8,);
-		sol.iter_mut().for_each(|v| v.sort(),);
-		sol.sort();
+		let mut ans = 3;
+		let mut sol = Solution::first_missing_positive(vec![1, 2, 0],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![1, 2, 2], vec![5]];
-		let mut sol = Solution::combination_sum2(vec![2, 5, 2, 1, 2], 5,);
-		sol.iter_mut().for_each(|v| v.sort(),);
-		sol.sort();
+		let mut ans = 2;
+		let mut sol = Solution::first_missing_positive(vec![3, 4, -1, 1],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_3() {
-		let mut ans: Vec<Vec<i32,>,> = vec![];
-		let mut sol = Solution::combination_sum2(vec![1], 2,);
-		sol.iter_mut().for_each(|v| v.sort(),);
-		sol.sort();
-		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn test_4() {
-		let mut ans =
-			vec![vec![1, 1, 2, 2], vec![1, 1, 4], vec![1, 2, 3], vec![2, 2, 2], vec![2, 4]];
-		let mut sol = Solution::combination_sum2(vec![4, 4, 2, 1, 4, 2, 2, 1, 3], 6,);
-		sol.iter_mut().for_each(|v| v.sort(),);
-		sol.sort();
+		let mut ans = 1;
+		let mut sol = Solution::first_missing_positive(vec![7, 8, 9, 11, 12],);
 		assert_eq!(ans, sol);
 	}
 }
