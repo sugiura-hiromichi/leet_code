1f8188d125d7f1c043a190d0f34bcba7eba5ca20
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-03 02:07:54 +0900

    solutions(42) rt[0 100] mem[2.1 88]

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -4,53 +4,18 @@ struct Solution;
 impl Solution {
 	pub fn trap(height: Vec<i32,>,) -> i32 {
 		let len = height.len();
-		let hold_mxp = {
-			let mut ret = 0;
-			for i in 1..len {
-				if height[ret] < height[i] {
-					ret = i;
-				}
-			}
-			ret
-		};
-		let (mut mxp, mut water,) = (hold_mxp, 0,);
+		let (mut water, mut l, mut r, mut max,) = (0, 0, len - 1, 0,);
 
-		// d: at first, sum up range of 0..mxp
-		while mxp > 1 {
-			let ano = {
-				let mut ret = 0;
-				for i in 1..mxp {
-					if height[ret] < height[i] {
-						ret = i;
-					}
-				}
-				ret
-			};
-			for i in ano..mxp {
-				water += height[ano] - height[i];
+		while l < r {
+			max = max.max(height[l].min(height[r],),);
+			if height[l] > height[r] {
+				r -= 1;
+				water += 0.max(max - height[r],);
+			} else {
+				l += 1;
+				water += 0.max(max - height[l],);
 			}
-			mxp = ano;
 		}
-
-		// d: second, sum up range of mxp..len
-		mxp = hold_mxp + 1;
-		while mxp < len {
-			let ano = {
-				let mut ret = mxp;
-				for i in mxp..len {
-					if height[ret] <= height[i] {
-						ret = i;
-					}
-				}
-				ret
-			};
-			for i in mxp..ano {
-				water += height[ano] - height[i];
-			}
-
-			mxp = ano + 1;
-		}
-
 		water
 	}
 }
@@ -66,20 +31,24 @@ mod tests {
 		assert_eq!(ans, sol);
 	}
 
-	//FAIL: infinite loop
 	#[test]
 	fn test_2() {
-		let mut ans = 0;
-		let mut sol = Solution::trap(vec![3, 4, 1, 1],);
+		let mut ans = 8;
+		let mut sol = Solution::trap(vec![5, 8, 9, 4, 9, 6, 1, 4],);
 		assert_eq!(ans, sol);
 	}
 
+	// FAIL: sol=5
 	#[test]
 	fn test_3() {
-		let mut ans = 3;
-		let mut sol = Solution::trap(vec![9, 7, 8, 11, 12],);
+		let mut ans = 12;
+		let mut sol = Solution::trap(vec![2, 8, 5, 5, 6, 1, 7, 4, 5],);
 		assert_eq!(ans, sol);
 	}
 }
 
-fn main() {}
+fn main() {
+	// water 0, i 5, l 1, r 8
+	// water 4, i 7, l 1, r 8
+	Solution::trap(vec![2, 8, 5, 5, 6, 1, 7, 4, 5],);
+}
