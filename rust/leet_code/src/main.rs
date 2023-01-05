f1452ecb154ea1d26fa14aa4d45ae81873105769
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-05 10:30:56 +0900

    solutions(43) simple strategy, fast

--- rust/leet_code/src/main.rs
+++ rust/leet_code/src/main.rs
@@ -2,53 +2,45 @@
 
 struct Solution;
 impl Solution {
-	pub fn trap(height: Vec<i32,>,) -> i32 {
-		let len = height.len();
-		let (mut water, mut l, mut r, mut max,) = (0, 0, len - 1, 0,);
-
-		while l < r {
-			max = max.max(height[l].min(height[r],),);
-			if height[l] > height[r] {
-				r -= 1;
-				water += 0.max(max - height[r],);
-			} else {
-				l += 1;
-				water += 0.max(max - height[l],);
+	pub fn multiply(num1: String, num2: String,) -> String {
+		let (len1, len2,) = (num1.len(), num2.len(),);
+		let mut digits = vec![0; len1 + len2];
+		for (i1, c1,) in num1.chars().rev().enumerate() {
+			for (i2, c2,) in num2.chars().rev().enumerate() {
+				let n1 = c1 as u8 - '0' as u8;
+				let n2 = c2 as u8 - '0' as u8;
+				let rslt = n1 * n2 + digits[i1 + i2];
+				digits[i1 + i2] = rslt % 10;
+				digits[i1 + i2 + 1] += rslt / 10;
 			}
 		}
-		water
+
+		while digits.len() > 1 && digits.last() == Some(&0,) {
+			digits.pop();
+		}
+
+		digits.iter().rev().map(|d| d.to_string(),).collect()
 	}
 }
 
+// t: This is Test Module
 #[cfg(test)]
 mod tests {
 	use super::*;
 
 	#[test]
 	fn test_1() {
-		let mut ans = 6;
-		let mut sol = Solution::trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],);
+		let mut ans = "6";
+		let mut sol = Solution::multiply("2".to_string(), "3".to_string(),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = 8;
-		let mut sol = Solution::trap(vec![5, 8, 9, 4, 9, 6, 1, 4],);
-		assert_eq!(ans, sol);
-	}
-
-	// FAIL: sol=5
-	#[test]
-	fn test_3() {
-		let mut ans = 12;
-		let mut sol = Solution::trap(vec![2, 8, 5, 5, 6, 1, 7, 4, 5],);
+		let mut ans = "56088";
+		let mut sol = Solution::multiply("123".to_string(), "456".to_string(),);
 		assert_eq!(ans, sol);
 	}
 }
 
-fn main() {
-	// water 0, i 5, l 1, r 8
-	// water 4, i 7, l 1, r 8
-	Solution::trap(vec![2, 8, 5, 5, 6, 1, 7, 4, 5],);
-}
+fn main() {}
