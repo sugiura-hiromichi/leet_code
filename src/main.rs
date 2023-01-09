4211d5d7437195f4db753b24658642816a0bd0fd
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-09 17:37:57 +0900

    accepted(46) rt[0 100] mem[2.4 14]

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,21 @@
 
 struct Solution;
 impl Solution {
-	pub fn jump(nums: Vec<i32,>,) -> i32 {
-		let (mut fur, mut end, mut ret, l1,) = (0, 0, 0, nums.len(),);
-		for i in 0..l1 - 1 {
-			fur = fur.max(i + nums[i] as usize,);
-			if i == end {
-				ret += 1;
-				end = fur;
-			}
+	pub fn permute(nums: Vec<i32,>,) -> Vec<Vec<i32,>,> {
+		let len = nums.len();
+		if len == 1 {
+			return vec![nums];
 		}
+		let mut ret = vec![];
+		for i in 0..len {
+			let mut cl = nums.clone();
+			let rm = cl.remove(i,);
+			Self::permute(cl,).iter_mut().for_each(|v| {
+				v.push(rm,);
+				ret.push(v.clone(),);
+			},)
+		}
+
 		ret
 	}
 }
@@ -22,22 +28,22 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = 2;
-		let mut sol = Solution::jump(vec![2, 3, 1, 1, 4],);
+		let mut ans = vec![vec![1, 2, 3], vec![1, 3, 2], vec![2, 1, 3], vec![2, 3, 1], vec![3, 1, 2], vec![3, 2, 1]];
+		let mut sol = Solution::permute(vec![1, 2, 3],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = 2;
-		let mut sol = Solution::jump(vec![2, 3, 0, 0, 4],);
+		let mut ans = vec![vec![0, 1], vec![1, 0]];
+		let mut sol = Solution::permute(vec![0, 1],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_3() {
-		let mut ans = 2;
-		let mut sol = Solution::jump(vec![1, 2, 0, 1],);
+		let mut ans = vec![vec![1]];
+		let mut sol = Solution::permute(vec![1],);
 		assert_eq!(ans, sol);
 	}
 }
