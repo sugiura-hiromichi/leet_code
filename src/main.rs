b706c889bb4a89371d1892a270af3a0ca059f2a0
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-10 16:57:52 +0900

    accepted(47) rt[78 6] mem[2.2 55]

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@
 
 struct Solution;
 impl Solution {
-	pub fn permute(nums: Vec<i32,>,) -> Vec<Vec<i32,>,> {
+	pub fn permute_unique(nums: Vec<i32,>,) -> Vec<Vec<i32,>,> {
 		let len = nums.len();
 		if len == 1 {
 			return vec![nums];
 		}
-		let mut ret = vec![];
+		let mut ret = std::collections::HashSet::new();
 		for i in 0..len {
 			let mut cl = nums.clone();
 			let rm = cl.remove(i,);
-			Self::permute(cl,).iter_mut().for_each(|v| {
+			Self::permute_unique(cl,).iter_mut().for_each(|v| {
 				v.push(rm,);
-				ret.push(v.clone(),);
+				ret.insert(v.clone(),);
 			},)
 		}
 
-		ret
+		ret.iter().map(|v| v.clone(),).collect()
 	}
 }
 
@@ -29,21 +29,21 @@ mod tests {
 	#[test]
 	fn test_1() {
 		let mut ans = vec![vec![1, 2, 3], vec![1, 3, 2], vec![2, 1, 3], vec![2, 3, 1], vec![3, 1, 2], vec![3, 2, 1]];
-		let mut sol = Solution::permute(vec![1, 2, 3],);
+		let mut sol = Solution::permute_unique(vec![1, 2, 3],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![0, 1], vec![1, 0]];
-		let mut sol = Solution::permute(vec![0, 1],);
+		let mut ans = vec![vec![1, 1, 2], vec![1, 2, 1], vec![2, 1, 1]];
+		let mut sol = Solution::permute_unique(vec![1, 1, 2],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_3() {
 		let mut ans = vec![vec![1]];
-		let mut sol = Solution::permute(vec![1],);
+		let mut sol = Solution::permute_unique(vec![1],);
 		assert_eq!(ans, sol);
 	}
 }
