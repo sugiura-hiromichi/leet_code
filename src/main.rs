da8698ec3342abe1fd7407b9d79ea579593f5242
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-11 09:54:07 +0900

    accepted(48) rt[1 76] mem[2.2 16]

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,29 @@
 
 struct Solution;
 impl Solution {
-	pub fn permute_unique(nums: Vec<i32,>,) -> Vec<Vec<i32,>,> {
-		let len = nums.len();
-		if len == 1 {
-			return vec![nums];
+	pub fn rotate(mtrx: &mut Vec<Vec<i32,>,>,) {
+		let size = mtrx.len();
+		for i in 0..(size + 1) / 2 {
+			Self::swap(mtrx, size, i,);
 		}
-		let mut ret = std::collections::HashSet::new();
-		for i in 0..len {
-			let mut cl = nums.clone();
-			let rm = cl.remove(i,);
-			Self::permute_unique(cl,).iter_mut().for_each(|v| {
-				v.push(rm,);
-				ret.insert(v.clone(),);
-			},)
-		}
+	}
 
-		ret.iter().map(|v| v.clone(),).collect()
+	fn swap(mtrx: &mut Vec<Vec<i32,>,>, size: usize, swapped: usize,) {
+		let len = size - 1 - swapped * 2;
+		let i0 = (swapped, swapped,);
+		let i1 = (i0.0, len + i0.1,);
+		let i2 = (i1.1, i1.1,);
+		let i3 = (i1.1, i1.0,);
+		for l in 0..len {
+			//(mtrx[i0.0][i0.1 + l], mtrx[i1.0 + l][i1.1], mtrx[i2.0][i2.1 - l], mtrx[i3.0 - l][i3.1],) = (mtrx[i3.0 - l][i3.1], mtrx[i0.0][i0.1 + l], mtrx[i1.0 + l][i1.1], mtrx[i2.0][i2.1 - l],);
+
+         let tmp=mtrx[i0.0][i0.1+l];
+         mtrx[i0.0][i0.1+l]=mtrx[i3.0-l][i3.1];
+         mtrx[i3.0-l][i3.1]=mtrx[i2.0][i2.1-l];
+         mtrx[i2.0][i2.1-l]=mtrx[i1.0+l][i1.1];
+         mtrx[i1.0+l][i1.1]=tmp;
+         
+		}
 	}
 }
 
@@ -26,24 +33,21 @@ impl Solution {
 mod tests {
 	use super::*;
 
+	fn vectorize(ary: &[&[i32]],) -> Vec<Vec<i32,>,> { ary.iter().map(|a| a.to_vec(),).collect() }
+
 	#[test]
 	fn test_1() {
-		let mut ans = vec![vec![1, 2, 3], vec![1, 3, 2], vec![2, 1, 3], vec![2, 3, 1], vec![3, 1, 2], vec![3, 2, 1]];
-		let mut sol = Solution::permute_unique(vec![1, 2, 3],);
+		let mut ans = vectorize(&[&[7, 4, 1,], &[8, 5, 2,], &[9, 6, 3,],],);
+		let mut sol = vectorize(&[&[1, 2, 3,], &[4, 5, 6,], &[7, 8, 9,],],);
+		Solution::rotate(&mut sol,);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![1, 1, 2], vec![1, 2, 1], vec![2, 1, 1]];
-		let mut sol = Solution::permute_unique(vec![1, 1, 2],);
-		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn test_3() {
-		let mut ans = vec![vec![1]];
-		let mut sol = Solution::permute_unique(vec![1],);
+		let mut ans = vectorize(&[&[15, 13, 2, 5,], &[14, 3, 4, 1,], &[12, 6, 8, 9,], &[16, 7, 10, 11,],],);
+		let mut sol = vectorize(&[&[5, 1, 9, 11,], &[2, 4, 8, 10,], &[13, 3, 6, 7,], &[15, 14, 12, 16,],],);
+		Solution::rotate(&mut sol,);
 		assert_eq!(ans, sol);
 	}
 }
