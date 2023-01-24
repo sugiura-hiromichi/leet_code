c3efb06cc0032ceea2efa189fdba8ca2cdea9578
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-24 17:44:07 +0900

    accepted(56) rt[8 69] mem[3 53]

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,21 @@ const EPSILON: f64 = 1e-10;
 
 struct Solution;
 impl Solution {
-	// p: just scan 0 and element which can jump beyond 0. if such element not found, return false
-	pub fn can_jump(nums: Vec<i32,>,) -> bool {
-		let mut n = 1;
-		for i in (0..nums.len() - 1).rev() {
-			n = if nums[i] < n { n + 1 } else { 1 };
+	pub fn merge(mut nums: Vec<Vec<i32,>,>,) -> Vec<Vec<i32,>,> {
+		nums.sort();
+		let mut rslt = vec![vec![nums[0][0], nums[0][1]]];
+		let mut rs_i = 0;
+		for i in 1..nums.len() {
+			if rslt[rs_i][1] < nums[i][0] {
+				rslt.push(nums[i].clone(),);
+				rs_i += 1;
+			} else {
+				if rslt[rs_i][1] < nums[i][1] {
+					rslt[rs_i][1] = nums[i][1];
+				}
+			}
 		}
-		n == 1
+		rslt
 	}
 }
 
@@ -21,27 +29,17 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = true;
-		let mut sol = Solution::can_jump(vec![2, 3, 1, 1, 4],);
+		let mut ans = vec![vec![1, 6], vec![8, 10], vec![15, 18]];
+		let mut sol = Solution::merge(vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = false;
-		let mut sol = Solution::can_jump(vec![3, 2, 1, 0, 4],);
+		let mut ans = vec![vec![1, 5]];
+		let mut sol = Solution::merge(vec![vec![1, 4], vec![4, 5]],);
 		assert_eq!(ans, sol);
 	}
-
-	#[test]
-	fn test_3() {
-		let mut ans = true;
-		let mut sol = Solution::can_jump(vec![5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0],);
-		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn split_tes() { let empty: &[i32] = &[]; }
 }
 
 fn main() {}
