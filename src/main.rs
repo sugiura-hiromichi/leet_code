68a464705c0c61e8b87dc31876111e9d99dd40b4
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-26 08:41:44 +0900

    accepted(57) rt[0 100] mem[2.8 41]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,80 @@
 #![allow(unused)]
 
 const EPSILON: f64 = 1e-10;
-
 struct Solution;
+
+#[derive(Clone,)]
+enum InsPos {
+	Left(usize,),
+	Between(usize,),
+	Right,
+}
+
+impl InsPos {
+	fn unwrap(self, len: usize,) -> usize {
+		match self {
+			Self::Right => len,
+			Self::Between(n,) | Self::Left(n,) => n,
+		}
+	}
+}
+
 impl Solution {
-	pub fn merge(mut nums: Vec<Vec<i32,>,>,) -> Vec<Vec<i32,>,> {
-		nums.sort();
-		let mut rslt = vec![vec![nums[0][0], nums[0][1]]];
-		let mut rs_i = 0;
-		for i in 1..nums.len() {
-			if rslt[rs_i][1] < nums[i][0] {
-				rslt.push(nums[i].clone(),);
-				rs_i += 1;
-			} else {
-				if rslt[rs_i][1] < nums[i][1] {
-					rslt[rs_i][1] = nums[i][1];
-				}
+	pub fn insert(itv: Vec<Vec<i32,>,>, new_itv: Vec<i32,>,) -> Vec<Vec<i32,>,> {
+		let len = itv.len();
+		if len == 0 {
+			return vec![new_itv];
+		}
+
+		let mut rslt = vec![];
+		let (start, end,) = (new_itv[0], new_itv[1],);
+		let (mut s, mut e,) = (InsPos::Right, InsPos::Right,);
+
+		for i in 1..itv.len() {
+			if itv[i][0] <= start && start <= itv[i][1] {
+				s = InsPos::Between(i,);
+			} else if itv[i - 1][1] < start && start < itv[i][0] {
+				s = InsPos::Left(i,);
+			}
+			if itv[i][0] <= end && end <= itv[i][1] {
+				e = InsPos::Between(i,);
+			} else if itv[i - 1][1] < end && end < itv[i][0] {
+				e = InsPos::Left(i,);
 			}
 		}
+
+		if start < itv[0][0] {
+			s = InsPos::Left(0,);
+		} else if itv[0][0] <= start && start <= itv[0][1] {
+			s = InsPos::Between(0,);
+		}
+		if itv[0][0] <= end && end <= itv[0][1] {
+			e = InsPos::Between(0,);
+		} else if end < itv[0][0] {
+			e = InsPos::Left(0,);
+		}
+
+		for i in 0..s.clone().unwrap(len,) {
+			rslt.push(itv[i].clone(),);
+		}
+
+		let start = match s {
+			InsPos::Between(i,) => itv[i][0],
+			_ => start,
+		};
+		let end = match e {
+			InsPos::Between(i,) => {
+				e = InsPos::Between(i + 1,);
+				itv[i][1]
+			},
+			_ => end,
+		};
+
+		rslt.push(vec![start, end],);
+		for i in e.unwrap(len,)..len {
+			rslt.push(itv[i].clone(),);
+		}
+
 		rslt
 	}
 }
@@ -29,15 +86,18 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = vec![vec![1, 6], vec![8, 10], vec![15, 18]];
-		let mut sol = Solution::merge(vec![vec![1, 3], vec![2, 6], vec![8, 10], vec![15, 18]],);
+		let mut ans = vec![vec![1, 5], vec![6, 9]];
+		let mut sol = Solution::insert(vec![vec![1, 3], vec![6, 9]], vec![2, 5],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![1, 5]];
-		let mut sol = Solution::merge(vec![vec![1, 4], vec![4, 5]],);
+		let mut ans = vec![vec![1, 2], vec![3, 10], vec![12, 16]];
+		let mut sol = Solution::insert(
+			vec![vec![1, 2], vec![3, 5], vec![6, 7], vec![8, 10], vec![12, 16]],
+			vec![4, 8],
+		);
 		assert_eq!(ans, sol);
 	}
 }
