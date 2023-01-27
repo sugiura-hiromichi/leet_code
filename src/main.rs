1167cbe3316286eb4f95f0de418ff705b1dcff6e
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-27 11:08:43 +0900

    accepted(58) rt[2 15] mem[2.1 72]

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
 // •‿•
 #![allow(unused)]
 struct Solution;
-use std::cmp::Ordering;
 impl Solution {
-	pub fn insert(itv: Vec<Vec<i32,>,>, new_itv: Vec<i32,>,) -> Vec<Vec<i32,>,> {
-		let len = itv.len();
-		let (mut s, mut e,) = (new_itv[0], new_itv[1],);
-
-		let li = itv.binary_search_by(Self::pred(s,),).unwrap_or_else(|x| x,);
-		let ri = itv.binary_search_by(Self::pred(e,),).unwrap_or_else(|x| x,);
-		let l = &itv[..(li + usize::from(li < len && itv[li][1] < s,))];
-		let r = &itv[(ri + usize::from(ri >= len || itv[ri][0] <= e,)).min(len,)..];
-
-		// q: what case is cared about in this `if`?
-		// -> this is required to determine range of `vec![s, e]`
-		if l.len() + r.len() != len {
-			s = s.min(itv[l.len()][0],);
-			e = e.max(itv[len - r.len() - 1][1],);
+	pub fn length_of_last_word(s: String,) -> i32 {
+		let mut i = s.len();
+		while i > 0 && &s[i - 1..i] == " " {
+			i -= 1;
 		}
 
-		vec![l, &vec![vec![s, e]], r].concat()
-	}
-
-	fn pred(x: i32,) -> impl Fn(&Vec<i32,>,) -> Ordering {
-		move |i: &Vec<i32,>| {
-			if i[0] > x {
-				Ordering::Greater
-			} else if i[1] < x {
-				Ordering::Less
-			} else {
-				Ordering::Equal
-			}
+		let tmp = i;
+		while i > 0 && &s[i - 1..i] != " " {
+			i -= 1;
 		}
+		(tmp - i) as i32
 	}
 }
 
@@ -42,27 +23,17 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = vec![vec![1, 5], vec![6, 9]];
-		let mut sol = Solution::insert(vec![vec![1, 3], vec![6, 9]], vec![2, 5],);
+		let mut ans = 5;
+		let mut sol = Solution::length_of_last_word("Hello World".to_string(),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![1, 2], vec![3, 10], vec![12, 16]];
-		let mut sol = Solution::insert(
-			vec![vec![1, 2], vec![3, 5], vec![6, 7], vec![8, 10], vec![12, 16]],
-			vec![4, 8],
-		);
+		let mut ans = 4;
+		let mut sol = Solution::length_of_last_word("        fly me	t the 	moo ".to_string(),);
 		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn usize_from() {
-		assert_eq!(usize::from(true), 1);
-		assert_eq!(usize::from(false), 0);
 	}
 }
 
 fn main() {}
-
