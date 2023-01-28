37b8fb4160fb2adb454d29071c411bc23e456855
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-28 19:37:24 +0900

    accepted() rt[] mem[]

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,53 @@
 // •‿•
 #![allow(unused)]
+
+use std::fmt::format;
+
 struct Solution;
 impl Solution {
-	pub fn generate_matrix(n: i32,) -> Vec<Vec<i32,>,> {
-		let mut rslt = vec![vec![0; n as usize]; n as usize];
-		let mut e = 1;
-		for i in 0..n as usize / 2 {
-			let len = n as usize - 2 * i - 1;
-			//top row
-			for j in 0..len {
-				rslt[i][i + j] = e;
-				e += 1;
-			}
-			//right col
-			for j in 0..len {
-				rslt[i + j][len + i] = e;
-				e += 1
-			}
-			//bottom row
-			for j in 0..len {
-				rslt[len + i][len + i - j] = e;
-				e += 1;
-			}
-			//left col
-			for j in 0..len {
-				rslt[len + i - j][i] = e;
-				e += 1;
-			}
+	pub fn get_permutation(n: i32, k: i32,) -> String {
+		let (n, mut k,) = (n as usize, k as usize - 1,);
+
+		let mut asc = vec![1; n]; // asc stand for ascending
+		let mut frac = 1;
+		for i in 2..=n {
+			frac *= i;
+			asc[i - 1] = i;
 		}
 
-		if n % 2 == 1 {
-			rslt[n as usize / 2][n as usize / 2] = e;
+		let mut rslt = "".to_string();
+		for i in 0..n {
+			frac /= n - i;
+			//println!("k / tmp | {k} / {tmp}");
+			rslt.push_str(&asc.remove(k / frac,).to_string(),);
+			k %= frac;
 		}
 		rslt
 	}
 }
 
-// t: This is Test Module
 #[cfg(test)]
 mod tests {
 	use super::*;
 
 	#[test]
 	fn test_1() {
-		let mut ans = vec![vec![1, 2, 3], vec![8, 9, 4], vec![7, 6, 5]];
-		let mut sol = Solution::generate_matrix(3,);
+		let mut ans = "213";
+		let mut sol = Solution::get_permutation(3, 3,);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![1]];
-		let mut sol = Solution::generate_matrix(1,);
+		let mut ans = "2314";
+		let mut sol = Solution::get_permutation(4, 9,);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_3() {
+		let mut ans = "123";
+		let mut sol = Solution::get_permutation(3, 1,);
 		assert_eq!(ans, sol);
 	}
 }
