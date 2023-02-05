ab3e392bf5b6fbc5b9f21e2af826101aaeb08af7
sugiura-hiromichi <sugiura130418@icloud.com>
2023-02-05 10:21:12 +0900

    accepted(71) rt[2 59] mem[2.1 69]

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,63 @@ use test::Bencher;
 
 struct Solution;
 impl Solution {
-	// fibonacci
-	pub fn climb_stairs(x: i32,) -> i32 {
-		(0..x).fold((1, 0,), |(rslt, prev,), _| (rslt + prev, rslt,),).0
+	pub fn simplify_path(mut p: String,) -> String {
+		p.push('/',);
+		// treat duplicated slash
+		Self::remove_dup_slash(&mut p,);
+		// remove `./`
+		Self::remove_explicit_curdir(&mut p,);
+		// remove `../`
+		Self::remove_explicit_parent_dir(&mut p,);
+
+		if p.len() == 1 {
+			p
+		} else {
+			p.pop();
+			p
+		}
+	}
+
+	fn remove_dup_slash(p: &mut String,) {
+		let mut i = 0;
+		while i < p.len() - 1 {
+			if &p[i..=i] == "/" {
+				let mut j = i + 1;
+				while j < p.len() && &p[j..=j] == "/" {
+					j += 1;
+				}
+				p.replace_range(i + 1..j, "",);
+			}
+			i += 1;
+		}
+	}
+
+	fn remove_explicit_curdir(p: &mut String,) {
+		let mut i = 0;
+		while p.len() > 2 && i < p.len() - 2 {
+			if &p[i..i + 3] == "/./" {
+				p.replace_range(i..i + 2, "",);
+			} else {
+				i += 1;
+			}
+		}
+	}
+
+	fn remove_explicit_parent_dir(p: &mut String,) {
+		let mut i = 0;
+		while p.len() > 3 && i < p.len() - 3 {
+			if &p[i..i + 4] == "/../" {
+				if i == 0 {
+					p.replace_range(i..i + 3, "",);
+				} else {
+					let start = p[..i].rfind('/',).unwrap();
+					p.replace_range(start..i + 3, "",);
+					i = start;
+				}
+			} else {
+				i += 1;
+			}
+		}
 	}
 }
 
@@ -20,15 +74,22 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = 2;
-		let mut sol = Solution::climb_stairs(2,);
+		let mut ans = "/home";
+		let mut sol = Solution::simplify_path("/home/".to_string(),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = 5;
-		let mut sol = Solution::climb_stairs(4,);
+		let mut ans = "/";
+		let mut sol = Solution::simplify_path("/.".to_string(),);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_3() {
+		let mut ans = "/home/foo";
+		let mut sol = Solution::simplify_path("/home//foo/".to_string(),);
 		assert_eq!(ans, sol);
 	}
 }
@@ -48,4 +109,7 @@ mod benchs {
 	}
 }
 
-fn main() { assert_eq!(21 - 15 % 6, 18) }
+fn main() {
+	let root = Solution::simplify_path("/.././.../././".to_string(),);
+	assert_eq!("/...", root);
+}
