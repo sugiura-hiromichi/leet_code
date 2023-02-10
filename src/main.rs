151bf44e7cb5ee9626be8b577b37ac64d7424d32
sugiura-hiromichi <sugiura130418@icloud.com>
2023-02-11 08:43:03 +0900

    solutions(72) cant solve by myself

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,25 @@
 #![allow(unused)]
 #![feature(test)]
 
-extern crate test;
-use test::black_box;
-use test::Bencher;
-
 struct Solution;
 impl Solution {
-	pub fn simplify_path(path: String,) -> String {
-		format!(
-			"/{}",
-			path.split('/')
-				.filter(|&s| s != "" && s != ".")
-				.fold(vec![], |mut acc, s| {
-					if s == ".." {
-						acc.pop();
-					} else {
-						acc.push(s,);
-					}
-					acc
-				})
-				.join("/")
-		)
+	pub fn min_distance(w1: String, w2: String,) -> i32 {
+		Self::rec(&w1, &w2, w1.len(), w2.len(),) as i32
+	}
+
+	fn rec(w1: &String, w2: &String, len1: usize, len2: usize,) -> usize {
+		if len1 == 0 {
+			len2
+		} else if len2 == 0 {
+			len1
+		} else if w1[len1 - 1..len1] == w2[len2 - 1..len2] {
+			Self::rec(w1, w2, len1 - 1, len2 - 1,)
+		} else {
+			let ins = Self::rec(w1, w2, len1, len2 - 1,);
+			let del = Self::rec(w1, w2, len1 - 1, len2,);
+			let rep = Self::rec(w1, w2, len1 - 1, len2 - 1,);
+			ins.min(del.min(rep,),) + 1
+		}
 	}
 }
 
@@ -32,28 +30,31 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = "/home";
-		let mut sol = Solution::simplify_path("/home/".to_string(),);
+		let mut ans = 3;
+		let mut sol = Solution::min_distance("horse".to_string(), "ros".to_string(),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = "/";
-		let mut sol = Solution::simplify_path("/.".to_string(),);
+		let mut ans = 5;
+		let mut sol = Solution::min_distance("intention".to_string(), "execution".to_string(),);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_3() {
-		let mut ans = "/home/foo";
-		let mut sol = Solution::simplify_path("/home//foo/".to_string(),);
+		let mut ans = 1;
+		let mut sol = Solution::min_distance("a".to_string(), "".to_string(),);
 		assert_eq!(ans, sol);
 	}
 }
 
 mod benchs {
+	extern crate test;
 	use super::*;
+	use test::black_box;
+	use test::Bencher;
 
 	//	#[bench]
 	fn b1(b: &mut Bencher,) {
