b702651a11ebb0829ee560a0f84c85aa43224e21
sugiura-hiromichi <sugiura130418@icloud.com>
2023-03-12 17:08:33 +0900

    accepted(77) rt[99 6] mem[40 8]

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,35 @@
 #![allow(unused)]
 #![feature(test)]
 
+/// doc
 struct Solution;
 impl Solution {
-	pub fn min_window(s: String, t: String,) -> String {
-		let s_bytes = s.as_bytes();
-		type ChGraph = std::collections::HashMap<u8, i32,>;
-		let mut t_chars = t.bytes().fold(ChGraph::new(), |mut hm, b| {
-			*hm.entry(b,).or_default() -= 1;
-			hm
-		},);
-
-		let (mut rslt, mut l,) = ((0, 0,), 0,);
-		for r in 1..=s.len() {
-			match t_chars.get_mut(&s_bytes[r - 1],) {
-				None => continue,
-				Some(i,) => {
-					*i += 1;
-				},
+	pub fn combine(n: i32, k: i32,) -> Vec<Vec<i32,>,> {
+		let mut rslt = vec![];
+		if k == 1 {
+			for i in 1..=n {
+				rslt.push(vec![i],);
 			}
-
-			while l < r {
-				if let Some(i,) = t_chars.get_mut(&s_bytes[l],) {
-					*i -= if *i > 0 { 1 } else { break };
-				}
-				l += 1;
-			}
-
-			if (0 == rslt.1 || r - l < rslt.1 - rslt.0) && t_chars.values().all(|&i| i >= 0,) {
-				rslt = (l, r,);
-			}
+			return rslt;
 		}
 
-		s[rslt.0..rslt.1].to_string()
+		Self::combine(n, k - 1,).iter().for_each(|v| {
+			for i in v[v.len() - 1] + 1..=n {
+				let mut tmp = v.clone();
+				tmp.push(i,);
+				rslt.push(tmp,);
+			}
+		},);
+		rslt
+	}
+
+	fn combination(n: i32, k: i32,) -> usize {
+		let mut rslt = 1;
+		for i in 0..k {
+			rslt *= (n - i);
+			rslt /= (i + 1);
+		}
+		rslt as usize
 	}
 }
 
@@ -43,47 +40,27 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = "BANC";
-		let mut sol = Solution::min_window("ADOBECODEBANC".to_string(), "ABC".to_string(),);
+		let mut ans = vec![vec![1, 2], vec![1, 3], vec![1, 4], vec![2, 3], vec![2, 4], vec![3, 4]];
+		let mut sol = Solution::combine(4, 2,);
+		ans.sort();
+		sol.sort();
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let mut ans = "aa";
-		let mut sol = Solution::min_window("aa".to_string(), "aa".to_string(),);
+		let mut ans = vec![vec![1]];
+		let mut sol = Solution::combine(1, 1,);
+		ans.sort();
+		sol.sort();
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
-	fn test_5() {
-		let mut ans = "a";
-		let mut sol = Solution::min_window("a".to_string(), "a".to_string(),);
-		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn test_3() {
-		let mut ans = "";
-		let mut sol = Solution::min_window("a".to_string(), "aa".to_string(),);
-		assert_eq!(ans, sol);
-	}
-
-	// FAIL:
-	#[test]
-	fn test_4() {
-		let mut ans = "babcbcacbbccbaccaaacacabbb";
-		let mut sol = Solution::min_window(
-			"acaabacababcbcacbbccbaccaaacacabbbcaccabccabbca".to_string(),
-			"bbaacbacbbccbcaabbacbacac".to_string(),
-		);
-		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn test_slice() {
-		let slice = "hehehe".to_string();
-		assert_eq!(&slice[0..0], "");
+	fn range_test() {
+		for i in 5..=4 {
+			assert!(true);
+		}
 	}
 }
 
