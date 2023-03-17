f3eda10dfd839f4b33785c35ebc0b1fec59e665f
sugiura-hiromichi <sugiura130418@icloud.com>
2023-03-17 09:50:40 +0900

    accepted(78) rt[0 100] mem[2.1 74]

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,32 @@
 /// doc
 struct Solution;
 impl Solution {
-	pub fn combine(n: i32, k: i32,) -> Vec<Vec<i32,>,> {
-		let mut rslt = vec![];
-		for i in 1..=n - k + 1 {
-			rslt.push(vec![i],);
+	pub fn subsets(nums: Vec<i32,>,) -> Vec<Vec<i32,>,> {
+		let (mut start, mut end,) = (0, 0,);
+		let mut rslt = nums.iter().fold(vec![], |mut acc, &x| {
+			acc.push(vec![x],);
+			end += 1;
+			acc
+		},);
+
+		let len = nums.len();
+		while rslt[end - 1].len() < len {
+			for i in start..end {
+				let ith = rslt[i].clone();
+				let mut pos =
+					nums.iter().enumerate().find(|&x| *x.1 == ith[ith.len() - 1],).unwrap().0;
+				while pos < len - 1 {
+					pos += 1;
+					let mut tmp = ith.clone();
+					tmp.push(nums[pos],);
+					rslt.push(tmp,);
+				}
+			}
+			start = end;
+			end = rslt.len();
 		}
 
-		for i in 1..k {
-			let mut cl = vec![];
-			rslt.iter().for_each(|v| {
-				for j in v[i as usize - 1] + 1..n - k + i + 2 {
-					let mut v = v.clone();
-					v.push(j,);
-					cl.push(v,);
-				}
-			},);
-			rslt = cl;
-		}
+		rslt.push(vec![],);
 		rslt
 	}
 }
@@ -32,8 +41,17 @@ mod tests {
 
 	#[test]
 	fn test_1() {
-		let mut ans = vec![vec![1, 2], vec![1, 3], vec![1, 4], vec![2, 3], vec![2, 4], vec![3, 4]];
-		let mut sol = Solution::combine(4, 2,);
+		let mut ans = vec![
+			vec![],
+			vec![1],
+			vec![2],
+			vec![1, 2],
+			vec![3],
+			vec![1, 3],
+			vec![2, 3],
+			vec![1, 2, 3],
+		];
+		let mut sol = Solution::subsets(vec![1, 2, 3],);
 		ans.sort();
 		sol.sort();
 		assert_eq!(ans, sol);
@@ -41,18 +59,11 @@ mod tests {
 
 	#[test]
 	fn test_2() {
-		let mut ans = vec![vec![1]];
-		let mut sol = Solution::combine(1, 1,);
+		let mut ans = vec![vec![], vec![0]];
+		let mut sol = Solution::subsets(vec![0],);
 		ans.sort();
 		sol.sort();
 		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn range_test() {
-		for i in 5..=4 {
-			assert!(true);
-		}
 	}
 }
 
@@ -65,7 +76,7 @@ mod benchs {
 	#[bench]
 	fn b1(b: &mut Bencher,) {
 		b.iter(|| {
-			let v = vec![0; 10000];
+			let v = vec![0; 100];
 			for i in 0..1000 {
 				black_box(v.last(),);
 			}
@@ -75,7 +86,7 @@ mod benchs {
 	#[bench]
 	fn b2(b: &mut Bencher,) {
 		b.iter(|| {
-			let v = vec![0; 10000];
+			let v = vec![0; 100];
 			for i in 0..1000 {
 				black_box(v[v.len() - 1],);
 			}
