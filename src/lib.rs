f77f3d173eec5a3beea3c8ac77cef66239cd2840
sugiura-hiromichi <sugiura130418@icloud.com>
2024-08-17 09:34:22 +0900

    accepted(81) rt[0 100] mem[2.13 52]

--- src/lib.rs
+++ src/lib.rs
@@ -4,35 +4,36 @@ struct Solution;
 impl Solution {
 	/// # Parameters
 	///
-	/// - `1 <= nums.len() <= 3*10^4`
+	/// - `1 <= nums.len() <= 5000`
 	/// - `-10^4 <= nums[i] <= 10^4`
-	/// - `nums` is sorted in non-decreasing order.
-	pub fn remove_duplicates(nums: &mut Vec<i32,>,) -> i32 {
-		let mut seq = 0;
-		let mut idx = 0;
+	/// - `nums` is sorted in non-decreasing order, then rotated at an unknown
+	///   pivot
+	/// - `-10^4 <= target <= 10^4`
+	pub fn search(nums: Vec<i32,>, target: i32,) -> bool {
+		let last = nums.len() - 1;
 
-		loop {
-			//evaluation
-
-			if seq >= 2 {
-				nums.remove(idx,);
-			} else {
-				idx += 1;
-			}
-
-			//update
-
-			if nums.len() == idx {
-				break;
-			}
-
-			if nums[idx] == nums[idx - 1] {
-				seq += 1;
-			} else {
-				seq = 0;
-			}
+		match target.cmp(&nums[0],) {
+			// target < nums[0]
+			std::cmp::Ordering::Less => {
+				let mut i = last;
+				loop {
+					match target.cmp(&nums[i],) {
+						std::cmp::Ordering::Less if i != 0 => i -= 1,
+						_ => return target == nums[i],
+					}
+				}
+			},
+			std::cmp::Ordering::Equal => true,
+			std::cmp::Ordering::Greater => {
+				let mut i = 0;
+				loop {
+					match target.cmp(&nums[i],) {
+						std::cmp::Ordering::Greater if i != last => i += 1,
+						_ => return target == nums[i],
+					}
+				}
+			},
 		}
-		idx as i32
 	}
 }
 
@@ -40,55 +41,38 @@ impl Solution {
 mod tests {
 	use super::*;
 
-	//	#[test]
-	//	fn test_1() {
-	//		let ans = true;
-	//		let sol = Solution::remove_duplicates(
-	//			vec![
-	//				vec!['A', 'B', 'C', 'E'],
-	//				vec!['S', 'F', 'C', 'S'],
-	//				vec!['A', 'D', 'E', 'E'],
-	//			],
-	//			"ABCCED".to_string(),
-	//		);
-	//		assert_eq!(ans, sol);
-	//	}
-	//
-	//	#[test]
-	//	fn test_2() {
-	//		let ans = true;
-	//		let sol = Solution::remove_duplicates(
-	//			vec![
-	//				vec!['A', 'B', 'C', 'E'],
-	//				vec!['S', 'F', 'C', 'S'],
-	//				vec!['A', 'D', 'E', 'E'],
-	//			],
-	//			"SEE".to_string(),
-	//		);
-	//		assert_eq!(ans, sol);
-	//	}
+	#[test]
+	fn test_1() {
+		let ans = true;
+		let sol = Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 0,);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_2() {
+		let ans = false;
+		let sol = Solution::search(vec![2, 5, 6, 0, 0, 1, 2], 3,);
+		assert_eq!(ans, sol);
+	}
 
 	#[test]
 	fn test_3() {
-		let ans = &mut vec![1, 1, 2, 2, 3];
-		let sol = &mut vec![1, 1, 1, 2, 2, 3];
-
-		let len = Solution::remove_duplicates(sol,) as usize;
-		assert_eq!(len, ans.len());
-		for i in 0..len {
-			assert_eq!(ans[i], sol[i]);
-		}
+		let ans = true;
+		let sol = Solution::search(vec![1, 0, 1, 1, 1], 0,);
+		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_4() {
-		let ans = &mut vec![0, 0, 1, 1, 2, 3, 3];
-		let sol = &mut vec![0, 0, 1, 1, 1, 1, 2, 3, 3];
+		let ans = true;
+		let sol = Solution::search(vec![1, 1, 1, 0, 1], 0,);
+		assert_eq!(ans, sol);
+	}
 
-		let len = Solution::remove_duplicates(sol,) as usize;
-		assert_eq!(len, ans.len());
-		for i in 0..len {
-			assert_eq!(ans[i], sol[i]);
-		}
+	#[test]
+	fn test_5() {
+		let ans = true;
+		let sol = Solution::search(vec![1, 1, 0, 1, 1], 0,);
+		assert_eq!(ans, sol);
 	}
 }
