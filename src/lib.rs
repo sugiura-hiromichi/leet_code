3aed35e6808fad8d4cf60f494ec0a4a09dd7ebc4
sugiura-hiromichi <sugiura130418@icloud.com>
2024-09-25 09:53:20 +0900

    accepted(84) rt[1441 6] mem[3.14 95]

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,45 @@
 #![allow(dead_code)]
 
-#[derive(PartialEq, Eq, Clone, Debug,)]
-pub struct ListNode {
-	pub val:  i32,
-	pub next: Option<Box<ListNode,>,>,
-}
-
-impl ListNode {
-	fn new(val: i32,) -> Self { ListNode { next: None, val, } }
-}
-
 struct Solution;
 impl Solution {
 	/// # Parameters
 	///
-	/// - The number of nodes in the list is [0,300]
-	/// - `-100<= Nodde.val <= 100`
+	/// - `1<=heights.len()<=10^5`
+	/// - `0<=heights[i] <= 10^4`
 	/// - list is guaranteed to be sorted in ascending order
-	pub fn delete_duplicates(
-		mut head: Option<Box<ListNode,>,>,
-	) -> Option<Box<ListNode,>,> {
-		// TODO: Consider using `Weak reference`
-		let mut p_list = &mut head;
+	pub fn largest_rectangle_area(heights: Vec<i32,>,) -> i32 {
+		let len = heights.len() - 1;
+		let mut rslt = 0;
 
-		while let Some(node,) = p_list {
-			if node.next.is_some() {
-				if node.val == node.next.as_ref().unwrap().val {
-					node.next = node.next.as_mut().unwrap().next.take();
-				} else {
-					p_list = &mut p_list.as_mut().unwrap().next;
+		// NOTE: When units are max, left edge index(assume `left`) satisfies
+		// `(left==0 || heights[left-1]<heights[left]) &&
+		// (left==heights.len()-1 || heights[left]<=heights[left+1])`
+		// collect indices which satisfies above condition
+		for i in 0..heights.len() {
+			if i == 0 || heights[i - 1] < heights[i]
+			//&& (i == heights.len() - 1 || heights[i] <= heights[i + 1])
+			{
+				let mut j = i;
+				let mut min = heights[i];
+				loop {
+					if heights[j] < min {
+						if (j - i) as i32 * min > rslt {
+							rslt = (j - i) as i32 * min;
+						}
+						min = heights[j];
+					}
+					if j == len {
+						if (j - i + 1) as i32 * min > rslt {
+							rslt = (j - i + 1) as i32 * min;
+						}
+						break;
+					}
+					j += 1;
 				}
-			} else {
-				break;
 			}
 		}
 
-		head
+		rslt
 	}
 }
 
@@ -43,28 +47,26 @@ impl Solution {
 mod tests {
 	use super::*;
 
-	fn create_list(from: &[i32],) -> Option<Box<ListNode,>,> {
-		if from.len() == 0 {
-			None
-		} else {
-			Some(Box::new(ListNode {
-				val:  from[0],
-				next: create_list(&from[1..],),
-			},),)
-		}
-	}
-
 	#[test]
 	fn test_1() {
-		let ans = create_list(&[1, 2,],);
-		let sol = Solution::delete_duplicates(create_list(&[1, 1, 2,],),);
+		let ans = 10;
+		let sol = Solution::largest_rectangle_area(vec![2, 1, 5, 6, 2, 3],);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let ans = create_list(&[1, 2, 3,],);
-		let sol = Solution::delete_duplicates(create_list(&[1, 1, 2, 3, 3,],),);
+		let ans = 4;
+		let sol = Solution::largest_rectangle_area(vec![2, 4],);
+		assert_eq!(ans, sol);
+	}
+
+	#[test]
+	fn test_3() {
+		let ans = 10;
+		let sol = Solution::largest_rectangle_area(vec![
+			10, 1, 1, 1, 1, 1, 1, 1, 1, 4,
+		],);
 		assert_eq!(ans, sol);
 	}
 }
