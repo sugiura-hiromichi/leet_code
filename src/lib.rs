0bbc200f06694a876c3b25e977b971130c3a0bf4
sugiura-hiromichi <sugiura130418@icloud.com>
2024-09-30 09:16:11 +0900

    accepted(86) rt[1 75] mem[2.1 73]

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 #![allow(dead_code)]
 
+
+#[derive(Debug, PartialEq, Eq, Clone,)]
+pub struct ListNode {
+	pub val:  i32,
+	pub next: List,
+}
+impl ListNode {
+	#[inline]
+	fn new(val: i32,) -> Self { ListNode { val, next: None, } }
+}
+
 struct Solution;
+type List = Option<Box<ListNode,>,>;
 impl Solution {
 	/// # Parameters
 	///
@@ -8,78 +20,25 @@ impl Solution {
 	/// - `cols == matrix.length`
 	/// - `1 <= row` `cols <= 200`
 	/// - `matrix[i][j] = '0' | '1'`
-	pub fn maximal_rectangle(matrix: Vec<Vec<char,>,>,) -> i32 {
-		let (r_max, c_max,) = (matrix.len(), matrix[0].len(),);
-		let (mut r, mut c,) = (0, 0,);
+	pub fn partition(head: List, x: i32,) -> List {
+		let (mut before, mut after,) = (None, None,);
+		let (mut before_tail, mut after_tail,) = (&mut before, &mut after,);
+		let mut p_head = &head;
 
-		let mut rslt = 0;
-		while r < r_max {
-			while c < c_max {
-				let mut heights = vec![];
-				if matrix[r][c] == '1' {
-					while c < c_max && matrix[r][c] == '1' {
-						let mut h = 1;
-						while r + h < r_max && matrix[r + h][c] == '1' {
-							h += 1;
-						}
-						heights.push(h,);
-						c += 1;
-					}
-					println!("{heights:?}");
-					rslt = Self::largest_rectangle_area(&heights,).max(rslt,);
-					if (r_max - r) * c_max <= rslt {
-						return rslt as i32;
-					}
-				}
-				c += 1;
+		while let Some(node,) = p_head {
+			if node.val < x {
+				*before_tail = Some(Box::new(ListNode::new(node.val,),),);
+				before_tail = &mut before_tail.as_mut().unwrap().next;
+			} else {
+				*after_tail = Some(Box::new(ListNode::new(node.val,),),);
+				after_tail = &mut after_tail.as_mut().unwrap().next;
 			}
-
-			c = 0;
-			r += 1;
+			p_head = &p_head.as_ref().unwrap().next;
 		}
 
-		rslt as i32
-	}
+		*before_tail = after;
 
-	/// # Parameters
-	///
-	/// - `1<=heights.len()<=10^5`
-	/// - `0<=heights[i] <= 10^4`
-	/// - list is guaranteed to be sorted in ascending order
-	pub fn largest_rectangle_area(heights: &Vec<usize,>,) -> usize {
-		let len = heights.len();
-
-		let mut left_less = vec![0; len];
-		let mut right_less = vec![0; len];
-		left_less[0] = -1;
-		right_less[len - 1] = len;
-
-		for i in 1..len {
-			let mut p = i as i32 - 1;
-			while p >= 0 && heights[p as usize] >= heights[i] {
-				p = left_less[p as usize];
-			}
-			left_less[i] = p;
-		}
-
-		for i in (0..len - 1).rev() {
-			let mut p = i + 1;
-			while p < len && heights[p] >= heights[i] {
-				p = right_less[p];
-			}
-			right_less[i] = p;
-		}
-
-		let mut rslt = 0;
-		for i in 0..len {
-			let rct =
-				heights[i] * (right_less[i] - (left_less[i] + 1) as usize);
-			if rct > rslt {
-				rslt = rct;
-			}
-		}
-
-		rslt
+		before
 	}
 }
 
@@ -87,29 +46,28 @@ impl Solution {
 mod tests {
 	use super::*;
 
+	fn create_list(ary: &[i32],) -> List {
+		if ary.len() == 0 {
+			None
+		} else {
+			Some(Box::new(ListNode {
+				val:  ary[0],
+				next: create_list(&ary[1..],),
+			},),)
+		}
+	}
+
 	#[test]
 	fn test_1() {
-		let ans = 6;
-		let sol = Solution::maximal_rectangle(vec![
-			vec!['1', '0', '1', '0', '0'],
-			vec!['1', '0', '1', '1', '1'],
-			vec!['1', '1', '1', '1', '1'],
-			vec!['1', '0', '0', '1', '0'],
-		],);
+		let ans = create_list(&[1, 2, 2, 4, 3, 5,],);
+		let sol = Solution::partition(create_list(&[1, 4, 3, 2, 5, 2,],), 3,);
 		assert_eq!(ans, sol);
 	}
 
 	#[test]
 	fn test_2() {
-		let ans = 0;
-		let sol = Solution::maximal_rectangle(vec![vec!['0']],);
-		assert_eq!(ans, sol);
-	}
-
-	#[test]
-	fn test_3() {
-		let ans = 1;
-		let sol = Solution::maximal_rectangle(vec![vec!['1']],);
+		let ans = create_list(&[1, 2,],);
+		let sol = Solution::partition(create_list(&[2, 1,],), 2,);
 		assert_eq!(ans, sol);
 	}
 }
